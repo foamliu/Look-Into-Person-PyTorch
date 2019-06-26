7b2b75ae25fa0b0336383a17eda785a11058c53f
Yang Liu <liuyang12@focusmedia.cn>
2019-06-26 16:55:51 +0800

    update

--- utils.py
+++ utils.py
@@ -102,6 +102,8 @@ def get_learning_rate(optimizer):
 def accuracy(scores, targets, k=1):
     batch_size = targets.size(0)
     _, ind = scores.topk(k, 1, True, True)
+    print(ind.size())
+    print(targets.size())
     correct = ind.eq(targets.view(-1, 1).expand_as(ind))
     correct_total = correct.view(-1).float().sum()  # 0D tensor
     return correct_total.item() * (100.0 / batch_size)
