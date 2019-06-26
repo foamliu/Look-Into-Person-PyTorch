7b487e004dfe1db6a0988eab822cb2e9cc07dbe4
Yang Liu <liuyang12@focusmedia.cn>
2019-06-26 15:53:16 +0800

    update

--- models.py
+++ models.py
@@ -4,6 +4,6 @@ from torchvision import models
 from config import im_size, device
 
 if __name__ == '__main__':
-    model = models.segmentation.deeplabv3_resnet50(pretrained=True, progress=True, num_classes=20)
+    model = models.segmentation.deeplabv3_resnet50(pretrained=False, progress=True, num_classes=20)
     model = model.to(device)
     summary(model, input_size=(3, im_size, im_size))
