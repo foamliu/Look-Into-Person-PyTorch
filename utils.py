fac45971aab984efa6ceaa1f1296737ea4b5b8e9
Yang Liu <liuyang12@focusmedia.cn>
2019-06-26 16:57:41 +0800

    update

--- utils.py
+++ utils.py
@@ -104,7 +104,7 @@ def accuracy(scores, targets, k=1):
     _, ind = scores.topk(k, 1, True, True)
     print(ind.size())
     print(targets.size())
-    correct = ind.eq(targets.view(-1, 1).expand_as(ind))
+    correct = ind.eq(targets.expand_as(ind))
     correct_total = correct.view(-1).float().sum()  # 0D tensor
     return correct_total.item() * (100.0 / batch_size)
 
