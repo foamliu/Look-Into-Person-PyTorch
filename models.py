afecc9951d7fe18c8e9f2ab32fda0032fe6bab27
Yang Liu <liuyang12@focusmedia.cn>
2019-06-26 15:52:01 +0800

    update

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from torchsummary import summary
 from torchvision import models
 
-from config import im_size
+from config import im_size, device
 
 if __name__ == '__main__':
     model = models.segmentation.deeplabv3_resnet50(pretrained=False, progress=True, num_classes=20)
+    model = model.to(device)
     summary(model, input_size=(3, im_size, im_size))
