c208dacdb1f7adf02d239e1e7d5da116f5d22a6a
Yang Liu <liuyang12@focusmedia.cn>
2019-07-02 09:24:26 +0800

    update

--- demo.py
+++ demo.py
@@ -44,7 +44,9 @@ if __name__ == '__main__':
         img = transformer(img)
         x_test[0:, 0:3, :, :] = img
 
-        out = model(x_test)['out']
+        with torch.no_grad():
+            out = model(x_test)['out']
+
         out = out.cpu().numpy()
         out = np.reshape(out, (im_size, im_size, num_classes))
         out = np.argmax(out, axis=2)
